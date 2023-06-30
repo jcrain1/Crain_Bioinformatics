85149a5ac728387fe5f708ea3670fa5a6d00e2db
Jared Crain <jcrain@ksu.edu>
2023-06-30 12:32:05 -0500

    concat

--- 01_Samples/scripts/BCL_Concat.sh
+++ 01_Samples/scripts/BCL_Concat.sh
@@ -14,10 +14,10 @@ ulimit -u 8192
 
 inputfile=$1
 inputdir=$2
-outputdir=$3
 
 mkdir -p $outputdir/01_Samples
 cd $inputdir/00_Demultiplex
+echo `pwd`
 
 echo "Doing JOB  on |" $(date) " | $inputfile | $inputdir "
 echo "" 
@@ -26,9 +26,10 @@ cat $inputfile | while read LINE; do
     
     echo "Working on $LINE"
     cat_files_R1=$(find . -type f -name "$LINE*R1_001.fastq.gz")
-   cat $cat_files_R1 > $inputdir/01_Samples/$LINE_R1.fastq.gz
-   cat_files_R2=$(find . -type f -name "$LINE*R2_001.fastq.gz")
-   cat $cat_files_R2 > $inputdir/01_Samples/$LINE_R2.fastq.gz
+    echo "$cat_files_R1"
+  # cat $cat_files_R1 > $inputdir/01_Samples/$LINE_R1.fastq.gz
+  # cat_files_R2=$(find . -type f -name "$LINE*R2_001.fastq.gz")
+  # cat $cat_files_R2 > $inputdir/01_Samples/$LINE_R2.fastq.gz
 
 
 done
