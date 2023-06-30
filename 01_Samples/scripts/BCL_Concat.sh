595d2f9506b3513995971f3e8b01bcf4e66ffd89
Jared Crain <jcrain@ksu.edu>
2023-06-30 13:00:10 -0500

    concat_shell

--- 01_Samples/scripts/BCL_Concat.sh
+++ 01_Samples/scripts/BCL_Concat.sh
@@ -27,7 +27,7 @@ cat $inputfile | while read LINE; do
     echo "Working on $LINE"
     cat_files_R1=$(find . -type f -name "${LINE}*R1_001.fastq.gz")
     echo "$cat_files_R1"
-    echo "$cat_files" | xargs cat > ${inputdir}/01_Samples/${LINE}_R1.fastq.gz
+    echo "$cat_files_R1" | xargs cat > ${inputdir}/01_Samples/${LINE}_R1.fastq.gz
    #cat ${cat_files_R1} > ${inputdir}/01_Samples/${LINE}_R1.fastq.gz
    #cat_files_R2=$(find . -type f -name "${LINE}*R2_001.fastq.gz")
   # echo "$cat_files_R2"
