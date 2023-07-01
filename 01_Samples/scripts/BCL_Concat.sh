e7d3315035a25874c374ee777f5801534d70a418
Jared Crain <jcrain@ksu.edu>
2023-06-30 20:38:01 -0500

    filter sam files

--- 01_Samples/scripts/BCL_Concat.sh
+++ 01_Samples/scripts/BCL_Concat.sh
@@ -12,9 +12,12 @@
 
 ulimit -u 8192
 
+####input variables full path to where the sample names are and output directory will be
 inputfile=$1
 inputdir=$2
 
+
+#make output directories and change
 mkdir -p $outputdir/01_Samples
 cd $inputdir/00_Demultiplex
 echo "Current working directory: `pwd`"
@@ -22,21 +25,26 @@ echo "Current working directory: `pwd`"
 echo "Doing JOB  on |" $(date) " | $inputfile | $inputdir "
 echo "" 
 
+#reads sample sheet line by line
 cat $inputfile | while read LINE; do
     
     echo "Working on $LINE"
+    #get all files and sorts them for the R1 file
     cat_files_R1=$(find . -type f -name "${LINE}*R1_001.fastq.gz" | sort)
     echo "$cat_files_R1"
+    #pulls the basename of the files
     r1_base=$(basename -a -s "_R1_001.fastq.gz" $cat_files_R1)
     
+    #concatenates multiple files together
     echo "$cat_files_R1" | xargs cat > ${inputdir}/01_Samples/${LINE}_R1.fastq.gz
    
-   
+   #work on R2 grab files and sort them
    cat_files_R2=$(find . -type f -name "${LINE}*R2_001.fastq.gz" | sort)
  echo "$cat_files_R2"
  r2_base=$(basename -a -s "_R2_001.fastq.gz" $cat_files_R2)
     echo "$cat_files_R2" | xargs cat > ${inputdir}/01_Samples/${LINE}_R2.fastq.gz
 
+#check if the file names in R1 and R2 are in correct order. If this is not equal proble.
 if [ "$r1_base" = "$r2_base" ]; then
     echo "Strings are equal."
 else
